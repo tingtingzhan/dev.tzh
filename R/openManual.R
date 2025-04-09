3520e1d3892ab30c5abb981c0737a2e7321f18c4
Tingting Zhan <tingtingzhan@gmail.com>
2025-04-09 10:29:11 -0400

    tzh

---
+++ R/openManual.R
@@ -0,0 +1,16 @@
+
+#' @title Open pdf Manual
+#' 
+#' @param x returned object from function \link[devtools]{build_manual}
+#' 
+#' @keywords internal
+#' @export
+openManual <- function(x) {
+  # `x` is the invisible return from ?devtools::build_manual
+  manual <- x$command |>
+    grep(pattern = '^--output=', value = TRUE) |>
+    gsub(pattern = '^--output=', replacement = '')
+  paste('open', manual) |> system() # no need to close the open pdf
+}
+
+
