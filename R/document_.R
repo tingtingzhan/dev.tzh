0fd23de04b5072bc90540e624a89ab5446ee463f
Tingting Zhan <tingtingzhan@gmail.com>
2025-04-09 09:53:03 -0400

    tzh

--- R/document_.R
+++ R/document_.R
@@ -1,76 +1,53 @@
 
 
-#' @title Auxiliary Tasks of \link[devtools]{document}
+#' @title Checke `.Rd` File
 #' 
 #' @description
-#' Auxiliary tasks of \link[devtools]{document}, including
-#' \itemize{
-#' \item {checking compliance to some conventions of CRAN}
-#' \item {\link[devtools]{spell_check}}
-#' \item {\link[devtools]{build_manual}}
-#' }
+#' Checking compliance to some conventions of CRAN.
 #' 
+#' @param path \link[base]{character} scalar, directory of one `.Rd` file
 #' 
-#' @param pkg \link[base]{character} scalar, directory of the package
-#' 
-#' @importFrom devtools build_manual document spell_check
-#' @importFrom utils capture.output
 #' @export
-document_ <- function(pkg = '.') {
-  pkg <- normalizePath(pkg)
-  name <- basename(pkg)
-  path <- dirname(pkg)
+check_Rd <- function(path) {
+  nm <- path |> basename()
+  txt <- path |> readLines()
   
-  # paste('open', pkg) |> system() # for my debug
-  suppressMessages(document(pkg = pkg, roclets = c('rd', 'collate', 'namespace'), quiet = FALSE))
-  # will add 'package:*name*' in base::search(), because of `load_code` parameter of ?roxygen2::roxygenize
-  detach(name = paste0('package:', name), unload = TRUE, character.only = TRUE) # otherwise multiple S4 definitions
+  idx <- grep(pattern = '^\\\\docType\\{.*\\}$', x = txt)
+  if (n <- length(idx)) {
+    if (n != 1L) stop('will not have more than one docType in one Rd file')
+    dTy <- txt[idx] |> gsub(pattern = '^\\\\docType\\{|\\}$', replacement = '')
+    switch(dTy, data = {
+      # do something
+    }, package = { # '\\docType{package}' still created by \CRANpkg{roxygen2}
+      # do something
+    }, class = { # S4 class
+      # do something
+    }, stop(sQuote(dTy), ' unknown?'))
+    return(invisible())
+  }
   
-  Rd <- list.files(path = file.path(pkg, 'man'), pattern = '\\.Rd$', full.names = TRUE)
-  lapply(Rd, FUN = \(i) { # (i = Rd[[11L]])
-    inm <- basename(i)
-    itxt <- readLines(con = i)
-    
-    idx <- grep('^\\\\docType\\{.*\\}$', x = itxt)
-    if (n <- length(idx)) {
-      if (n != 1L) stop('will not have more than one docType in one Rd file')
-      i_docType <- gsub(pattern = '^\\\\docType\\{|\\}$', replacement = '', x = itxt[idx])
-      switch(i_docType, data = {
-        # do something
-      }, package = { # '\\docType{package}' still created by \CRANpkg{roxygen2}
-        # do something
-      }, class = { # S4 class
-        # do something
-      }, stop(sQuote(i_docType), ' unknown?'))
-      return(invisible())
-    }
-    
-    is_internal <- ('\\keyword{internal}' %in% itxt)
-    
-    if (!is_internal) {
-      
-      # check '\value' fields in .Rd files (@returns in \pkg{roxygen2})
-      # \pkg{roxygen2} does not force this check, but CRAN personnel insists.
-      idx <- ('\\value{' == itxt)
-      if (sum(idx) > 1L) stop(sQuote(inm), ' has more than one @return')
-      if (!any(idx)) stop(sQuote(inm), ' does not have @return') # masked temporarily..
-      # no need; also do not want to import my [.cyan], etc.
-      # cat(c(sQuote(inm), 'has'), '@return', sQuote(itxt[which(idx) + 1L]), '\n\n')
-      
-    }
-    
-    # After R 4.0.0, \dontest{} is always run as part of CRAN submission (see ?devtools::check)
-    if (any('\\dontrun{' == itxt)) stop('Replace \\dontrun with \\donttest in ', sQuote(inm)) # masked temporarily
-    #if (any(grepl('microbenchmark', x = itxt)) && !any('library(microbenchmark)' == itxt)) stop('microbenchmark in ', sQuote(inm))
-  })
+  int <- ('\\keyword{internal}' %in% txt)
   
-  pkg |> spell_check(vignettes = FALSE) |> print()
-  cat('\n')
+  if (!int) {
+    
+    # check '\value' fields in .Rd files (@returns in \pkg{roxygen2})
+    # \pkg{roxygen2} does not force this check, but CRAN personnel insists.
+    idx <- ('\\value{' == txt)
+    if (sum(idx) > 1L) stop(sQuote(nm), ' has more than one @return')
+    if (!any(idx)) stop(sQuote(nm), ' does not have @return') # masked temporarily..
+    # no need; also do not want to import my [.cyan], etc.
+    # cat(c(sQuote(nm), 'has'), '@return', sQuote(txt[which(idx) + 1L]), '\n\n')
+    
+  }
   
-  capture.output(build_manual(pkg = pkg))
-  version <- read.dcf(file = file.path(pkg, 'DESCRIPTION'), fields = 'Version')
-  file.exists(pdf_manual <- file.path(path, paste0(name, '_', version, '.pdf')))
-  paste('open', pdf_manual) |> system() # no need to close the open pdf
+  # After R 4.0.0, \dontest{} is always run as part of CRAN submission (see ?devtools::check)
+  if (any('\\dontrun{' == txt)) stop('Replace \\dontrun with \\donttest in ', sQuote(nm)) # masked temporarily
+  #if (any(grepl('microbenchmark', x = txt)) && !any('library(microbenchmark)' == txt)) stop('microbenchmark in ', sQuote(nm))
+  
+  return(invisible())
 }
+
+
+
 
 
